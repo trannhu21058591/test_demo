8414c3cebc80a480ddae3dcd9d506ee892e4289a
Nhu <quynhnhu18121812@gmail.com>
2025-02-13 13:52:20 +0700

    test

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Quan trọng: giúp Tailwind quét các file React
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
